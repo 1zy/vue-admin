567e383c2bd3d382951818258646590bd8f40068
liao <1119642770@qq.com>
2020-10-13 23:12:19 +0800

    构建项目

---
+++ README.md
@@ -0,0 +1,3 @@
+#创建本地版号分支
+1.git checkout -b feature-vueAdmin-V1.0.0-20201005
+2.推送到远程：git push
---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="login">
+        2412512512
+        <el-button type="info">信息按钮</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+    background: #3445af;
+    height: 100vh;
+}
+</style>
+
